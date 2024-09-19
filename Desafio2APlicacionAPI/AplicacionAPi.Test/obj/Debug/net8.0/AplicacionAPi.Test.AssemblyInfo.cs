bb97e17f1d412c4c7e96217288c5a0c6ca29c224
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2024-09-18 22:40:54 -0600

    update string db

--- Desafio2APlicacionAPI/AplicacionAPi.Test/obj/Debug/net8.0/AplicacionAPi.Test.AssemblyInfo.cs
+++ Desafio2APlicacionAPI/AplicacionAPi.Test/obj/Debug/net8.0/AplicacionAPi.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacionAPi.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f9c2314ae899bb845d623a5074022c02ca80c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a79c922fbee4ef560de0be20abe1c609aaa0e58")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacionAPi.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacionAPi.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
